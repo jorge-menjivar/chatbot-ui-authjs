11cdaf8e9fb85e7f73587978d977a921a2deca9a
Jorge Menjivar <jorgemenjivar1997@gmail.com>
2023-05-12 01:38:32 -0700

    Support for more providers

---
+++ README.md
@@ -0,0 +1,64 @@
+# Chatbot UI Next Auth Extension
+
+This extension provides NextAuth SSO support to [Chatbot UI](https://github.com/jorge-menjivar/chatbot-ui).
+
+## Installation
+
+### Step 1
+
+Install this extension with npm:
+
+```sh
+npm i chatbot-ui-authjs@latest
+```
+
+### Step 2
+
+Turn on authentication for [Chatbot UI](https://github.com/jorge-menjivar/chatbot-ui) by setting the following env variable in `.env.local`. You may need to create the file.
+
+```sh
+NEXT_PUBLIC_AUTH_ENABLED=true
+```
+
+### Steps 3
+
+Add the IDs and client secrets of the providers you want to enable in the `env.local` file. See below.
+
+## Configuration
+
+When using this extension, the following environment variables can be set:
+
+| Environment Variable        | Default value           | Description                                                                                |
+| --------------------------- | ----------------------- | ------------------------------------------------------------------------------------------ |
+| NEXTAUTH_EMAIL_PATTERN      |                         | The email regex pattern granted access to chatbot-ui. For example `.+@mydomain.com`        |
+| NEXTAUTH_URL                | `http://localhost:3000` | NextAuth Settings. See [Official Document](https://next-auth.js.org/configuration/options) |
+| NEXTAUTH_SECRET             |                         | NextAuth Settings. See [Official Document](https://next-auth.js.org/configuration/options) |
+| \<PROVIDER\>\_CLIENT_ID     |                         | Provider OAuth Client ID                                                                   |
+| \<PROVIDER\>\_CLIENT_SECRET |                         | Provider OAuth Client Secret                                                               |
+
+Where provider is one of the following:
+
+- APPLE
+- AUTH0
+- COGNITO
+- DISCORD
+- FACEBOOK
+- GITHUB
+- GITLAB
+- GOOGLE
+- OKTA
+- REDDIT
+- SALESFORCE
+- SLACK
+- SPOTIFY
+- TWITCH
+- TWITTER
+
+For example, to enable Google and Github authentication, you would add the following to your `.env.local` file:
+
+```sh
+GITHUB_CLIENT_ID=xxxxxxxxxxxxx
+GITHUB_CLIENT_SECRET=xxxxxxxxxxxxx
+GOOGLE_CLIENT_ID=xxxxxxxxxxxxx
+GOOGLE_CLIENT_SECRET=xxxxxxxxxxxx
+```
--- src/api-handler.ts
+++ src/api-handler.ts
@@ -1,22 +1,70 @@
 import NextAuth, { NextAuthOptions } from 'next-auth';
+import AppleProvider from 'next-auth/providers/apple';
+import Auth0Provider from 'next-auth/providers/auth0';
+import CognitoProvider from 'next-auth/providers/cognito';
+import DiscordProvider from 'next-auth/providers/discord';
+import FacebookProvider from 'next-auth/providers/facebook';
 import GithubProvider from 'next-auth/providers/github';
+import GitlabProvider from 'next-auth/providers/gitlab';
 import GoogleProvider from 'next-auth/providers/google';
+import OktaProvider from 'next-auth/providers/okta';
+import RedditProvider from 'next-auth/providers/reddit';
+import SalesforceProvider from 'next-auth/providers/salesforce';
+import SlackProvider from 'next-auth/providers/slack';
+import SpotifyProvider from 'next-auth/providers/spotify';
+import TwitchProvider from 'next-auth/providers/twitch';
+import TwitterProvider from 'next-auth/providers/twitter';
 
+const authorization = {
+  params: {
+    prompt: 'login',
+    access_type: 'offline',
+    response_type: 'code',
+  },
+};
 const providers = [];
-if (process.env.GOOGLE_CLIENT_ID) {
+if (process.env.APPLE_CLIENT_ID) {
   providers.push(
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID!,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-
-      // Needed to be able to select a different google accounts each time.
-      authorization: {
-        params: {
-          prompt: 'login',
-          access_type: 'offline',
-          response_type: 'code',
-        },
-      },
+    AppleProvider({
+      clientId: process.env.APPLE_CLIENT_ID!,
+      clientSecret: process.env.APPLE_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+if (process.env.AUTH0_CLIENT_ID) {
+  providers.push(
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID!,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+if (process.env.COGNITO_CLIENT_ID) {
+  providers.push(
+    CognitoProvider({
+      clientId: process.env.COGNITO_CLIENT_ID!,
+      clientSecret: process.env.COGNITO_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+if (process.env.DISCORD_CLIENT_ID) {
+  providers.push(
+    DiscordProvider({
+      clientId: process.env.DISCORD_CLIENT_ID!,
+      clientSecret: process.env.DISCORD_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+if (process.env.FACEBOOK_CLIENT_ID) {
+  providers.push(
+    FacebookProvider({
+      clientId: process.env.FACEBOOK_CLIENT_ID!,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET!,
+      authorization: authorization,
     }),
   );
 }
@@ -25,6 +73,92 @@ if (process.env.GITHUB_CLIENT_ID) {
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID!,
       clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+if (process.env.GITLAB_CLIENT_ID) {
+  providers.push(
+    GitlabProvider({
+      clientId: process.env.GITLAB_CLIENT_ID!,
+      clientSecret: process.env.GITLAB_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+if (process.env.GOOGLE_CLIENT_ID) {
+  providers.push(
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+if (process.env.OKTA_CLIENT_ID) {
+  providers.push(
+    OktaProvider({
+      clientId: process.env.OKTA_CLIENT_ID!,
+      clientSecret: process.env.OKTA_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+if (process.env.REDDIT_CLIENT_ID) {
+  providers.push(
+    RedditProvider({
+      clientId: process.env.REDDIT_CLIENT_ID!,
+      clientSecret: process.env.REDDIT_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+if (process.env.SALESFORCE_CLIENT_ID) {
+  providers.push(
+    SalesforceProvider({
+      clientId: process.env.SALESFORCE_CLIENT_ID!,
+      clientSecret: process.env.SALESFORCE_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+
+if (process.env.SLACK_CLIENT_ID) {
+  providers.push(
+    SlackProvider({
+      clientId: process.env.SLACK_CLIENT_ID!,
+      clientSecret: process.env.SLACK_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+
+if (process.env.SPOTIFY_CLIENT_ID) {
+  providers.push(
+    SpotifyProvider({
+      clientId: process.env.SPOTIFY_CLIENT_ID!,
+      clientSecret: process.env.SPOTIFY_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+
+if (process.env.TWITCH_CLIENT_ID) {
+  providers.push(
+    TwitchProvider({
+      clientId: process.env.TWITCH_CLIENT_ID!,
+      clientSecret: process.env.TWITCH_CLIENT_SECRET!,
+      authorization: authorization,
+    }),
+  );
+}
+
+if (process.env.TWITTER_CLIENT_ID) {
+  providers.push(
+    TwitterProvider({
+      clientId: process.env.TWITTER_CLIENT_ID!,
+      clientSecret: process.env.TWITTER_CLIENT_SECRET!,
+      authorization: authorization,
     }),
   );
 }
